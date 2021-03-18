d87981d576b79b54b4834d7da35fe1a55a1cb0db
stefan0982 <stefan.c.0608@mail.ru>
2021-03-18 17:50:07 +0200

    hidden lagging test

--- src/components/RightContainer/RightContainer.js
+++ src/components/RightContainer/RightContainer.js
@@ -54,6 +54,8 @@ export default function RightContainer() {
   // de simplificat sistema aceasta
   // prin schimbarea doar la link cu if = toate, link to / else slug(categorie)
 
+  console.log(loading)
+
   if (loading) {
     return <p>loading</p>
   }
